c85c253c0658f17995bc7c56f6b0534f849a137c
Nathalie Brown <me@nathalieb.dev>
2021-11-22 20:42:53 -0500

    added JSON fetch and html formatting

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+  this.fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      let astronautList = "";
+      let container = document.querySelector("#container");
+      console.log(container);
+
+      for (astro of json) {
+        astronautList += `
+            <div class="astronaut">
+            <div class="bio">
+            <h3>${astro.firstName} ${astro.lastName}</h3>
+            <ul>
+                <li>Hours in space: ${astro.hoursInSpace}</li>
+                <li>Active: ${astro.active}</li>
+                <li>Skills: ${astro.skills.join(", ")}</li>
+            </ul>
+            </div>
+            <img class="avatar" src=${astro.picture}>
+        </div>
+        `;
+      }
+      container.innerHTML = astronautList;
+    });
+});
